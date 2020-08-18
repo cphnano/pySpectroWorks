1bcf437430d2cf50813438e018a7c7038e889190
Vinojan Iruthayam <14731156+vino-dk@users.noreply.github.com>
2020-08-18 15:20:10 +0200

    initial commit

---
+++ api_test.py
@@ -0,0 +1,27 @@
+import pyspectroworks
+from datetime import datetime
+
+conn = pyspectroworks.connect('INSERT_API_KEY_HERE')  # change this to match your API key
+
+# list projects
+print('PROJECTS: ')
+print('{: >3} {: >24} {: >24}'.format(' ', 'NAME', 'CREATED'))
+projects = conn.get_projects()
+for i, project in enumerate(projects):
+    timestamp = datetime.utcfromtimestamp(project.created/1000).strftime('%Y-%m-%d %H:%M:%S')
+    print('{: >3} {: >24} {: >24} '.format(i, project.project_name, timestamp))
+
+print('\n\nOPENING PROJECT "{}":'.format(projects[0].project_name))
+# load all files in project
+print('{: >6}{: >2}\t{: >8}'.format('CUVETTE', ' ', 'REFRACTIVE INDEX'))
+items = projects[0].get_items()
+for item in items:
+    res = item.results.get('refractive_index', {})
+    ri = res.get('value', 'N/A')
+    print('{: >6}{:0>2}\t{: >8}'.format(item.box_code, item.cuvette_idx, ri))
+
+# get spectrum of a particular item
+print('\n\nLOADING SPECTRUM: ')
+item = items[0]
+spectrum = item.get_spectrum('sample_B')
+print(spectrum)
---
+++ pyspectroworks.py
@@ -0,0 +1,79 @@
+import json
+import requests
+
+
+class Connection:
+    def __init__(self, api_key, url):
+        self.api_key = api_key
+        self.url = url
+        self.projects = None
+
+    def get_projects(self):
+        # load projects
+        if self.projects is None:
+            res = requests.get(self.url + 'list_projects', params={'api_key': self.api_key})
+            projects = json.loads(res.text)['message']['items']
+            self.projects = [Project(self, project) for project in projects]
+
+        return self.projects
+
+
+class Project:
+    def __init__(self, connection, data=None):
+        self.connection = connection
+        if data is None:
+            data = {}
+        self.data = data
+        self.project_name = data.get('project_name', '')
+        self.num_files = int(data.get('num_files', 0))
+        self.created = float(data.get('created', 0))
+        self.modified = float(data.get('modified', 0))
+        self.items = None
+
+    def get_items(self):
+        # load items
+        if self.items is None:
+            conn = self.connection
+            res = requests.get(conn.url + 'list_files_by_project',
+                               params={'api_key': conn.api_key, 'project_id': int(self.data['project_id'])})
+            file_list = json.loads(res.text)['message']['items']
+
+            file_ids = [file['file_id'] for file in file_list]
+            res = requests.post(conn.url + 'get_files',
+                                params={'api_key': conn.api_key}, data=json.dumps(file_ids))
+            self.items = [Item(self.connection, item) for item in json.loads(res.text)['message']['items']]
+
+        return self.items
+
+
+class Item:
+    def __init__(self, connection, data=None):
+        self.connection = connection
+        if data is None:
+            data = {}
+        self.data = data
+        self.created = float(data.get('created', 0))
+        self.modified = float(data.get('modified', 0))
+        self.cuvette_idx = int(data.get('cuvette_idx', 0))
+        self.box_code = data.get('box_code', 'XXXXXX')
+        self.results = data.get('results', {})
+
+    def get_spectrum(self, spectrum_type):
+        spectrum_names = {
+            'reference_b': 'ref',
+            'sample_b': 'ri1',
+            'sample_d': 'ri2',
+            'sample_a': 'abs'
+        }
+        spectrum_name = spectrum_names[spectrum_type.lower()]
+        conn = self.connection
+        res = requests.get(conn.url + 'get_spectrum',
+                           params={'api_key': conn.api_key,
+                                   'file_id': int(self.data['file_id']),
+                                   'spectrum_types': ['spectrum_'+spectrum_name]})
+        return json.loads(res.text)['message']['spectrum_'+spectrum_name]['spectrum']
+
+
+def connect(api_key):
+    return Connection(api_key, 'https://api.spectroworks.com/dev/api/')
+
